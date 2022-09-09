c946673f6faa7cff33c2055382657291b4fad091
Jemima Okouya <ashbeca16@gmail.com>
2022-09-09 16:24:10 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of twom digits
+ * numbers must be separated by ,, followed by a space
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; m <= 57; n++)
+	{
+		for (m = 49; m <= 57; n++)
+		{
+		    if (m  > n)
+		    {
+			putchar(n);
+			putchar(m);
+			if (n != 56 || m != 57)
+			{
+				putchar (',');
+				putchar(' ');
+			}
+		   }
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
