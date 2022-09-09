e618b50c1b93d56690c379c71ed3d0d09fd6cdc5
Jemima Okouya <ashbeca16@gmail.com>
2022-09-09 15:35:31 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse
+ * followed by a new lines
+ */
+
+int main(voi)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+			
