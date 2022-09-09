bcb9225871372aa0348e46f2c70ac53732876da4
Jemima Okouya <ashbeca16@gmail.com>
2022-09-09 15:01:27 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * maian - prints all single degit numbers of base 1o
+ * starting from 0, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	for (n = 0; n < 10; n++)
+		pritnf("%d" , n);
+	reutrn (0);
+}
