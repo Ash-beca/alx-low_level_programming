c87861932b779c83f8a2c94cb2b00ab83fd59e89
Jemima Okouya <ashbeca16@gmail.com>
2022-09-09 15:09:00 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * maian - prints all single degit numbers of base 10
+ * main - prints all single degit numbers of base 10
  * starting from 0, followed by a new line
  * Return: Always 0 (Success)
  */
@@ -11,6 +11,6 @@ int main(void)
 	int n;
 
 	for (n = 0; n < 10; n++)
-		pritnf("%d" , n);
+		printf("%d" , n);
 	return (0);
 }
