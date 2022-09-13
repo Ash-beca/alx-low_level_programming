abf9fe4b92bfdbf99934c77ef1ce84aa3687b981
Jemima Okouya <ashbeca16@gmail.com>
2022-09-13 16:01:06 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * int add - adds two intergers
+ * @n1: interger 1
+ * @n2: interger 2
+ * Return: sum
+ */
+int add(int, int)
+{
+	int sum;
+
+	sum = n1 + n2;
+
+	reutrn (sum);
+}
