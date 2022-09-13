483a3af73bf689a56eedec2c87e03ad2436e23bf
Jemima Okouya <ashbeca16@gmail.com>
2022-09-13 05:47:45 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar 
+ * followed by a new line
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	putchar("_putchar\n"; _putchar);
+	return (0);
+}
