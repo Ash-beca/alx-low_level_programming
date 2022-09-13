4eddb39bedcd560453d0a4f97b7cca838db05174
Jemima Okouya <ashbeca16@gmail.com>
2022-09-13 07:05:59 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+ * main - prints the alphabet in lowercase
+ * followed bt a new line
+ * Return: Always 0 
+ */
+
+void print_alphabet(void);
+{
+
+
+
+
+
+
+
+
+
+   return (0);
+}
