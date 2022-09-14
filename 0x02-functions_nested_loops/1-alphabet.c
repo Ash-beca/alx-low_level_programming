f8903a31c9ab7295af249473950323ecd0957255
Jemima Okouya <ashbeca16@gmail.com>
2022-09-14 00:28:06 -0700

    updated fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,9 +8,9 @@
 
 void print_alphabet(void)
 {
-	char letter;
+	char letter = 'a';
 	
-	for (letter = 'a'; letter <= 'z'; letter++);
+	while (letter <= 'z');
         {
 	    _putchar(letter);
 	    letter++;
