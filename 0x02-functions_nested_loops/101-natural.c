7143029386ad81e6baa2fbee4b06a4dc8bf0d7df
Jemima Okouya <ashbeca16@gmail.com>
2022-09-14 03:03:42 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - list all natural numbers multiples by 3 & 5
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int num, sum;
+	sum = o;
+
+	for (num = 0, num < 1024; num++);
+	{
+		if (num % 3 == 0 || num % 5 == 0)
+		{
+			sum += num;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
