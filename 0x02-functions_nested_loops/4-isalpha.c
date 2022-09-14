e50f447f2c85af83309b4249732113dd55fbb303
Jemima Okouya <ashbeca16@gmail.com>
2022-09-14 01:57:06 -0700

    update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,24 @@
 
 /**
  * int _isalpha - checks for alphabetic character
+ * @c: c is an ascii character
+ *
  * Returns: 1 if c is a letter, lowercase or uppercase
- * Returns: 0 otherwise
+ * otherwise 0
  */
 
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
