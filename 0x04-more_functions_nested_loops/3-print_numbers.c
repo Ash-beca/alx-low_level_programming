4f213f3b8c30ea99189431ee477ddc82bafd9b23
Jemima Okouya <ashbeca16@gmail.com>
2022-09-15 14:33:29 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints the number from 0 to 9, followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a 
+	
+	if (a = '0'; a > '10'; a++)
+	{
+		_puthcar (a);
+	}
+	
+	_putchar ('\n');
+
+	return (0);
+}
+
