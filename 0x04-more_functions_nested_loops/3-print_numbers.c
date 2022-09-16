ea698713a284ac4a79beaddf07a902f0c8271981
Jemima Okouya <ashbeca16@gmail.com>
2022-09-15 20:44:14 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,15 @@
 
 /**
  * print_numbers - prints the number from 0 to 9, followed by a new line
+ * Return: integers
  */
 
 void print_numbers(void)
 {
-	int a = '0'; 
-	
-	while(a <= '9')
-	{
+	int a;
+
+	for ( a = 0; a < 10; a++)
 		_puthcar (a + '0');
-		a++;
-	}
 	_putchar ('\n');
 
 }
