3cf9d157b94d1bbb23b6c227c1ea9ab8d393fb99
Jemima Okouya <ashbeca16@gmail.com>
2022-09-15 20:33:08 -0700

    55

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,14 @@ void more_numbers(void)
 
 	for (num1 = 0; num1 < 10; num1 = num1 + 1)
 	{
-		if (num2 > 9)
+		for (num2 = 0; num2 <= 14; num2 = num2 +1)
 		{
-			_putchar(num2 / 10 + '0');
+			if (num2 > 9)
+			{
+				_putchar(num2 / 10 + '0');
+			}
+			_putchar(num2 % 10 + '0');
 		}
-		_putchar(num2 % 10 + '0');
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
