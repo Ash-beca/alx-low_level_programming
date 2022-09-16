a5c86afde92a7d38b0ec7c8156b4a9b0083580de
Jemima Okouya <ashbeca16@gmail.com>
2022-09-15 20:39:37 -0700

    all

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,10 +8,12 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if (i != 2 && i != 4)
-			_puthcar(i + '0');
+		if (i != '2' && i != '4')
+		{
+			_puthcar(i);
+		}
 	}
 	_putchar('\n');
 }
