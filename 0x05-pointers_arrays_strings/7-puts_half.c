133da47ba7e24b2d4613bc6a70caccc0831cc2c7
Jemima Okouya <ashbeca16@gmail.com>
2022-09-19 12:22:37 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,13 +11,14 @@ void puts_half(char *str)
 	int i, j, max;
 
 	i = 0;
-	max = i;
-	j = max / 2;
 
 	while (str[i] != '\0')
 	{
 		i++;
 	}
+	max = i;
+	j = max / 2;
+
 	while (j <= max)
 	{
 		_putchar(str[j]);
