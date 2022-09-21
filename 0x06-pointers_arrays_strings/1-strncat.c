758576da9b27eab5cdbd6e161f3e01bffec66306
Jemima Okouya <ashbeca16@gmail.com>
2022-09-21 12:03:19 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,19 +9,6 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int x = 0;
-
-	while (dest[x] != '\0')
-	{
-		x++;
-	}
-
-	for (i - 0 ; i < n && src[i] != '\0' ; i++)
-	{
-		dest[x + i] = src[x];
-	}
-	dest[x + i] = '\0';
-
+	strncat(dest, src, n);
 	return (dest);
 }
