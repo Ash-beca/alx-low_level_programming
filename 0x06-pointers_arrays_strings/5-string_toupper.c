085cd7db69863aa171f16281d0b9e36fc7ec0739
Jemima Okouya <ashbeca16@gmail.com>
2022-09-22 10:07:41 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,19 +2,19 @@
 
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase
- * @x: string
+ * @upper: input of lowercase to uppercase
  * Return: string
  */
 
 char *string_toupper(char *x)
 {
-	int i = 0;
+	int i;
 
-	while (upper[i] != '\0'; i++)
+	for (i = 0; upper[i] != '\0'; i++)
 	{
 		if (upper[i] >= 'a' && upper[i] <= 'z')
 		{
-			upper[i] = upper[i] - 95;
+			upper[i] = upper[i] - 32;
 		}
 	}
 	return (upper);
