db064b30e0ef24f29e27deb0526105eb3f5ae0cf
Jemima Okouya <ashbeca16@gmail.com>
2022-09-22 09:56:48 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,10 +11,10 @@ void reverse_array(int *a, int n)
 	int i;
 	int y;
 
-	for (i = 0; i < n/ 2; i++)
+	for (i = 0; i < n / 2; i++)
 	{
 		y = a[i];
-		a[i] = a[n -i - 1];
+		a[i] = a[n - i - 1];
 		a[n - i -  1] = y;
 
 	}
