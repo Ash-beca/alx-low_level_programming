d45a83a6ada51d627e556fd078414262813cb84f
Jemima Okouya <ashbeca16@gmail.com>
2022-09-22 12:40:58 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,18 +9,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int len = 0, i;
+	int i = 0;
+	int y = 0;
 
-
-	while (dest[len])
-		len++;
-
-	for (i = 0; src[i] != 0; i++)
-	{
-		dest[len] = src[i];
-		len += 1;
-	}
-	dest[len] = '\0';
+	while (dest[i++])
+		y++;
+	for (i = 0; src[i]; i++)
+		dest[y++] = src[i];
 
 	return (dest);
 }
