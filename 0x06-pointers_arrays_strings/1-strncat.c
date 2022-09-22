e7319d9759a924663348037d89a2d1960aaf6a44
Jemima Okouya <ashbeca16@gmail.com>
2022-09-22 09:18:05 -0700

    fixing errors

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,8 +8,17 @@
  * @n: number of bytes from src
  * Return: new string
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, n);
+	int i = 0;
+	int y = 0;
+
+	while (dest[i++])
+		y++;
+
+	for (i = 0; src[i] && i < n; i++)
+		dest[y++] = src[i];
+
 	return (dest);
 }
