6c8ce7c4d3697cf8c168a5c417c2d7aa12b2318f
Jemima Okouya <ashbeca16@gmail.com>
2022-09-27 10:49:03 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,7 +4,7 @@
 /**
  * _strpbrk - searches a string for any of a set of byte
  * @s: string to search
- * @accpet: string containing the byte
+ * @accept: string containing the byte
  *
  * Return:  a pointer to the byte in s that matches one of the bytes in accept,
  * or NULL if no such byte is found
