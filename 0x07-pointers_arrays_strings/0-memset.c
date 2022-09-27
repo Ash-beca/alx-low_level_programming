8186bbc6f357a57e7979e1ab71ea7a159ed8ad52
Jemima Okouya <ashbeca16@gmail.com>
2022-09-27 09:28:39 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,14 +5,14 @@
  * @s: string
  * @b: constant byte
  * @n: byte
- * Return: s
+ * Return: pointer to s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	if (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b
 	}
