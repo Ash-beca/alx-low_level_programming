f19954c247415f6be6daf0cd5975c0a3ed9549e2
Jemima Okouya <ashbeca16@gmail.com>
2022-10-05 12:57:10 -0700

    argc_argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,10 +5,10 @@
  * main - prints the number of arguments passed into it.
  * @argc: argument count
  * @argv: argument vector
- * Return: 0 
+ * Return: 0
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argv[0])
 	{
