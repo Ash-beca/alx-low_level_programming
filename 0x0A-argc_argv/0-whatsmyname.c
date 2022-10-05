7c6559a676462fffcfc65538a699663f96830e2a
Jemima Okouya <ashbeca16@gmail.com>
2022-10-05 12:36:12 -0700

    argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <srdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: argument count
+ * @argv: argument vector
+ *Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	pritnf("%s\n", argv[0]);
+	
+	return (0);
+}
