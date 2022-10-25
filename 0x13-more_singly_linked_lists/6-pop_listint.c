5e034522670f0b2dbc0cbc387149a98b46b291f3
Jemima Okouya <ashbeca16@gmail.com>
2022-10-25 11:12:33 -0700

    singly linked links

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,24 +9,16 @@
 
 int pop_listint(listint_t **head)
 {
+	listint_t *tmp;
 	int n;
-	listint_t *first;
-	listint_t *next;
 
-	if (head == NULL)
-		return (0);
-
-	first = *head;
-
-	if (first == NULL)
-		return (0);
-
-	next = (*head)->next;
-
-	n = (*head)->n;
-	*head = next;
-	free(first);
-
+	tmp = *head;
+	n = 0;
+	if (*head == NULL)
+		return (n);
+	*head = tmp->next;
+	n = tmp->n;
+	free(tmp);
 	return (n);
 }
 
