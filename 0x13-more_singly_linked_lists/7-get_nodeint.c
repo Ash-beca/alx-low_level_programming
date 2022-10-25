bc9a2725a0eb565e610471b90265d953db0e9f21
Jemima Okouya <ashbeca16@gmail.com>
2022-10-25 09:30:04 -0700

    singly linked links

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,7 +4,7 @@
  * get_nodeint_at_index - returns the nth node of a listint_t linked list
  * @head: first element
  * @index: element number
- * Return: a node 
+ * Return: a node
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
