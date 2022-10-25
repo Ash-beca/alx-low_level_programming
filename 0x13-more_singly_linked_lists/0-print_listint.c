7370d22631fe90b2df7f77092b785c3722ecf02f
Jemima Okouya <ashbeca16@gmail.com>
2022-10-24 23:52:49 -0700

    singly linked links

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,15 @@
+#include "list.h"
+
+/**
+ * print_listint - prints all the element of a listint_t list
+ * @h: list to search
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	if (h)
+		return (0);
+	printf("%d\n", h->n);
+	return (print_listint(h->next) + 1);
+}
