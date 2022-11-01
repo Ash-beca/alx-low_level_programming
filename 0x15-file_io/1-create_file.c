151ade1dc1738561b55aa42b2e78d06c4a3c1538
Jemima Okouya <ashbeca16@gmail.com>
2022-11-01 11:00:02 -0700

    file io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,12 +18,12 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fn = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fn == -1)
 		return (-1);
 
-	for (i = 0; text_content(i); i++);
+	for (i = 0; text_content[i]; i++);
 
 	rw = write(fn, text_content, i);
 
