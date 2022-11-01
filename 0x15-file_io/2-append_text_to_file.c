ba6212620954706c71d9c18df25fde7b0276cd7c
Jemima Okouya <ashbeca16@gmail.com>
2022-11-01 12:05:46 -0700

    file io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,15 +20,17 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	fn = open(filename, O_WRONLY | O_APPEND);
 
-	if (text_content)
-	{
-		for (i = 0; text_content[i]; i++)
+	if (text_content == NULL)
+		return (1);
+	
+	for (i = 0; text_content[i]; i++)
 			;
-		rw = write(fn, text_content, i);
-
-		if (rw == -1)
-			return (-1);
-	}
+	
+	rw = write(fn, text_content, i);
+	
+	if (rw == -1)
+		return (-1);
+	
 	close(fn);
 
 	return (1);
