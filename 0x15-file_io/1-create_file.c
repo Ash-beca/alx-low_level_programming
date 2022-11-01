71ba1c88bf5a6462941ab7af69064c7571630a5a
Jemima Okouya <ashbeca16@gmail.com>
2022-11-01 11:01:44 -0700

    file io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,8 @@ int create_file(const char *filename, char *text_content)
 	if (fn == -1)
 		return (-1);
 
-	for (i = 0; text_content[i]; i++);
+	for (i = 0; text_content[i]; i++)
+		;
 
 	rw = write(fn, text_content, i);
 
