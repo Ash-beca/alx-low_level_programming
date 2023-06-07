ab75928395d9100571d41674c10a8b180afd2de3
Aisha <ash-beca16@gmail.com>
2023-06-07 02:29:16 -0700

    adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - prints the results followed by a new line
+ * @argv: contailns the program command line in arguments
+ * @argc: contains the number of line arguments
+ * Return: 0 - success
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, add = 0;
+
+	for (a = 1; i < argc; a++)
+	{
+		for (j = 0; argv[a][b] != '\0'; b++)
+		{
+		if (!isdigit(argv[a][b]))
+		{
+		printf("Error\n");
+		return (1);
+		}
+		}
+		add += atoi(argv[a]);
+	}
+	print("%d\n", add);
+	return (0);
+}
+		
